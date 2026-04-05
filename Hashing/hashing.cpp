4c0a7dcfb5a4263d69a843b2f1f24565e9b12beb
Harsh Pandey <harsh008110@gmail.com>
2026-04-05 19:05:43 +0530

    Hashing complete

--- Hashing/hashing.cpp
+++ Hashing/hashing.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // method 1
-
+/*
 int main()
 {
     int arr[10] = {0, 1, 2, 1, 2, 1, 2, 2, 1, 0};
@@ -30,5 +30,34 @@ int main()
     {
         cout << "The character " << char(i + 'a') << " appears " << hashh[i] << " times" << endl;
     }
+    return 0;
+}
+*/
+
+// method 2
+
+int main()
+{
+    int n;
+    cout << "Enter size of arrray" << endl;
+    cin >> n;
+    int arr[n] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the number: ";
+        cin >> arr[i];
+    }
+
+    map<long int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr[i]]++;
+    }
+    cout << "Hash Map created" << endl;
+
+    int number;
+    cout << "Enter the number: ";
+    cin >> number;
+    cout << mpp[number] << endl;
     return 0;
 }
