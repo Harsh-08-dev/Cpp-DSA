f4f207adab70a56510273e54bb49d41d35b6c51b
Harsh Pandey <harsh008110@gmail.com>
2026-05-30 22:57:27 +0530

    Added a folder and inside it a program named recursion to practice it

---
+++ Recursion/Recursion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void func1(x);
+int main()
+{
+    int x = 10;
+    void func1(x)
+    {
+        x = x - 1 cout << "Hello";
+        void func1(x - 1);
+    }
+    return 0;
+}
